3c19e03c762908548e78cb80143d8aaa223d1dec
Kaosi Anikwe <anikwehenryasa@gmail.com>
2026-02-14 13:15:16 +0100

    feat: style StartGameScreen with input container and shadow effects

--- 02-num-game/screens/StartGameScreen.js
+++ 02-num-game/screens/StartGameScreen.js
@@ -1,12 +1,27 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    padding: 16,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});

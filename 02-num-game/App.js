cb6c846bc184d6326dcdc2350b2ef03b45295ee4
Kaosi Anikwe <anikwehenryasa@gmail.com>
2026-02-18 15:21:24 +0100

    feat: implement GameOverScreen and integrate game over logic in GameScreen and App components

--- 02-num-game/App.js
+++ 02-num-game/App.js
@@ -5,18 +5,29 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 import GameScreen from "./screens/GameScreen";
 import Colors from "./constants/colors";
+import GameOverScreen from "./screens/GameOverScreen";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+
+  function gameOverHandler() {
+    setGameIsOver(true);
   }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
-  if (userNumber) screen = <GameScreen userNumber={userNumber} />;
+  if (userNumber)
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  if (gameIsOver && userNumber) screen = <GameOverScreen />;
 
   return (
     <SafeAreaProvider>

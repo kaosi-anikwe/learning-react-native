40717ea420f466e893b546240190e6e86feb2993
Kaosi Anikwe <anikwehenryasa@gmail.com>
2026-02-26 12:07:05 +0100

    feat: add ExpenseItem component and integrate with ExpensesList and ExpensesSummary

--- 05-expense-tracker/components/ExpensesOutput.js/ExpensesOutput.js
+++ 05-expense-tracker/components/ExpensesOutput.js/ExpensesOutput.js
@@ -1,7 +1,8 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
+import { GlobalStyles } from "../../constants/styles";
 
 const DUMMY_EXPENSES = [
   {
@@ -34,13 +35,47 @@ const DUMMY_EXPENSES = [
     amount: 18.59,
     date: new Date("2025-02-25"),
   },
+  {
+    id: "e6",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2025-01-19"),
+  },
+  {
+    id: "e7",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2024-12-10"),
+  },
+  {
+    id: "e8",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2024-12-15"),
+  },
+  {
+    id: "e9",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2025-02-25"),
+  },
 ];
 
 export default function ExpenseOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});

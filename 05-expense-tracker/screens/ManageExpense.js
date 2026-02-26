5efcc2a66892d9d4a0a0ef864bba443ef7b3aaaf
Kaosi Anikwe <anikwehenryasa@gmail.com>
2026-02-26 19:13:53 +0100

    feat: add LoadingOverlay component and integrate it into ManageExpense and RecentExpenses screens

--- 05-expense-tracker/screens/ManageExpense.js
+++ 05-expense-tracker/screens/ManageExpense.js
@@ -1,13 +1,15 @@
 import { StyleSheet, View } from "react-native";
-import { useContext, useLayoutEffect } from "react";
+import { useContext, useLayoutEffect, useState } from "react";
 
 import { GlobalStyles } from "../constants/styles";
 import IconButton from "../components/UI/IconButton";
 import { ExpensesContext } from "../store/expenses-context";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 import { storeExpense, updateExpense, deleteExpense } from "../util/http";
 
 export default function ManageExpense({ route, navigation }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const expensesCtx = useContext(ExpensesContext);
 
   const editedExpenseId = route.params?.expenseId;
@@ -24,7 +26,9 @@ export default function ManageExpense({ route, navigation }) {
   }, [navigation, isEditing]);
 
   async function deleteExpenseHandler() {
+    setIsSubmitting(true);
     await deleteExpense(editedExpenseId);
+    // setIsSubmitting(false)
     expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
@@ -34,6 +38,7 @@ export default function ManageExpense({ route, navigation }) {
   }
 
   async function confirmHandler(expenseData) {
+    setIsSubmitting(true);
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
       await updateExpense(editedExpenseId, expenseData);
@@ -42,6 +47,11 @@ export default function ManageExpense({ route, navigation }) {
       expensesCtx.addExpense({ ...expenseData, id });
     }
     navigation.goBack();
+    // setIsSubmitting(false)
+  }
+
+  if (isSubmitting) {
+    return <LoadingOverlay />;
   }
 
   return (
--- 05-expense-tracker/screens/RecentExpenses.js
+++ 05-expense-tracker/screens/RecentExpenses.js
@@ -1,20 +1,28 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { fetchExpenses } from "../util/http";
 import { getDateMinusDays } from "../util/date";
 import { ExpensesContext } from "../store/expenses-context";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import ExpenseOutput from "../components/ExpensesOutput.js/ExpensesOutput";
 
 export default function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
   const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
+      setIsFetching(false);
       expensesCtx.setExpenses(expenses);
     }
     getExpenses();
   }, []);
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();

82350333c9a86df58debd2dc89952c3aa155d42b
Kaosi Anikwe <anikwehenryasa@gmail.com>
2026-02-27 10:04:42 +0100

    feat(auth): implement user authentication with login and signup functionality
    - Added authentication utility functions for user creation and login using Firebase.
    - Updated LoginScreen to handle user login with loading state and error handling.
    - Updated SignupScreen to handle user registration with loading state and error handling.
    - Added necessary dependencies for navigation and safe area context in package.json.

--- 06-auth/screens/SignupScreen.js
+++ 06-auth/screens/SignupScreen.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import { Alert } from "react-native";
+
+import { createUser } from "../util/auth";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function sigupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user, please check your input and try again later.",
+      );
+    }
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+
+  return <AuthContent onAuthenticate={sigupHandler} />;
 }
 
 export default SignupScreen;
